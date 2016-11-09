cbd4f0a0c16d6a4b2d7f0a0629ed8cc84fc8326d
Greg Ritter <greg@lolay.com>
2016-11-09 08:46:27 -0800

    Initial Commit

---
+++ ImagePickerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePickerTest
+//
+//  Created by Gregory Ritter on 2016-11-09.
+//  Copyright © 2016 Lolay, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagePickerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePickerTest
+//
+//  Created by Gregory Ritter on 2016-11-09.
+//  Copyright © 2016 Lolay, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
